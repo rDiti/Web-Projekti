e8c22fe50f0ccb6c6455cb83cc45536630e427b3
rDiti <arditqirezi@gmail.com>
2024-12-01 11:59:44 +0100

    Nuk e di qa ndodhi fiks

--- contact.JavaScript.js
+++ contact.JavaScript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const forms = document.querySelectorAll("form");
 
@@ -32,39 +31,4 @@ forms.forEach((form) => {
       textreply.value = "";
     }
   }
-=======
-
-const forms = document.querySelectorAll("form");
-
-
-forms.forEach((form) => {
-  form.addEventListener("submit", function (e) {
-   
-    e.preventDefault();
-
- 
-    const textarea = form.getElementsById("textreply");
-    const message = textarea.value.trim(); 
-
-    
-    let responseMessage = form.querySelector("#responseMessage");
-    if (!responseMessage) {
-      responseMessage = document.createElement("p");
-      responseMessage.id = "responseMessage";
-      form.appendChild(responseMessage);
-    }
-
-    
-    if (message === "") {
-      responseMessage.textContent = "Duhesh me shkru diçka!";
-      responseMessage.style.color = "red";
-    } else {
-      responseMessage.textContent = "Mesazhi u dergua!";
-      responseMessage.style.color = "green";
-
-      
-      textreply.value = "";
-    }
-}
->>>>>>> 7eea70d98ae2b0f3ddffaeea1b7dd656c71f7489
 }
