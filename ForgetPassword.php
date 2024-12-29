1f54ab395615813e66bcc99995e91d0d6e55770a
rDiti <arditqirezi@gmail.com>
2024-12-29 14:49:26 +0100

    Ndryshimet e emrave te files edhe kthimi i tyre ne php

---
+++ ForgetPassword.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="ForgotPassword.css">
+  <title>Reset Password</title>
+</head>
+<body>
+  <div class="klasa1">
+    <a href="Homepage.html">
+      <img src="logo_transparent.png" alt="logo" class="logo">
+    </a>
+  </div>
+  <div class="form-klasa2">
+    <div class="form-klasa3">
+      <p>Enter your email or phone number. We will send you instructions to reset your password.</p>
+      <form>
+        <input type="email" placeholder="Email..." required>
+        <input type="telefon" placeholder="+383..." required>
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="cont.css">
+</head>
+<body>
+   <header>
+    <div class="container">
+        <a href="Homepage.html">
+            <img src="logo_transparent.png" alt="logo" class="logo">
+        </a>
+    </div>
+   </header>
+   <main>
+    <div class="message-box">
+        <p>Per qdo shqetesim apo problem qe e keni ne nderfaqen tone <br>
+        Mund te na kontaktoni , dhe do ju ndihmojme sa me shpejte qe te kemi mundsi.
+
+        </p>
+        <br>
+        <p > <strong>E-mail:</strong>cck-team@gmail.com</p>
+        <br>
+        <p><strong>Phone Number:</strong><i>+383/044/200/222</i></p>
+    </div>
+    <div class="contact-box">
+        <div class="email-box">
+            
+           
+            <form id="textbox">
+                <label for="textreply">Shkruaj emailin</label><br>
+                <textarea name="textreply" id="textreply" rows="2" cols="40" placeholder="Shkruaj problemin tuaj"></textarea><br>
+                <button type="submit">Dergo</button>
+               </form>
+
+        </div>
+       
+    </div>
+  
+   </main>
+   <script src="contact.JavaScript.js"></script>
+   
+</body>
+</html>
